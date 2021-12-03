d6e2a709565db2395023ebadae9bd010b9a6b19c
Zhi Qiang Zheng <zhizheng33782@gmail.com>
2021-12-03 18:21:25 -0500

    Add files via upload

---
+++ PNGtoJPEGconverter.py
@@ -0,0 +1,26 @@
+import sys
+import os
+from PIL import Image
+
+#Grab both source of png and destination for jpeg exists
+source = sys.argv[1]
+destination = sys.argv[2]
+
+#Check if the destination folder exist, if not then create it
+if not os.path.exists(destination):
+    os.makedirs(destination)
+
+#Loop through each image in source folder to convert png to jpg and save into destination folder
+for filename in os.listdir(source):
+    #Grabbing the png images from the source folder
+    img = Image.open(f'{source}{filename}')
+    #Convert the images to RGB color spaces
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    # splitext to make sure the filename doesn't contain .png when converting
+    clean_name = os.path.splitext(filename)[0]
+
+    #Converting the png to jpg and saving it to the destination
+    img.save(f'{destination}{clean_name}.jpg', 'JPEG')
+    print(f'Image {filename} is convert to {clean_name}.jpg')
+print('all done!')
